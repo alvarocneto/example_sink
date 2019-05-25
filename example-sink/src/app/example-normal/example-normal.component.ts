0ec1bb767eb8c5a1e7eea82172d2e9fc1fb7aced
Alvaro <alvaro.camilloneto@gmail.com>
2019-05-25 00:35:57 -0300

    Versão com o leak

--- example-sink/src/app/example-normal/example-normal.component.ts
+++ example-sink/src/app/example-normal/example-normal.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+
+import { People, Planet } from './../model/api';
 
 @Component({
   selector: 'app-example-normal',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./example-normal.component.css']
 })
 export class ExampleNormalComponent implements OnInit {
+  people: People;
+  planet: Planet;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
   ngOnInit() {
+    this.http
+      .get<People>('https://swapi.co/api/people/1/')
+      .subscribe((data: People) => {
+        this.people = data;
+      });
+    this.http
+      .get<Planet>('https://swapi.co/api/planets/3/')
+      .subscribe((data: Planet) => {
+        this.planet = data;
+      });
   }
-
 }
---
+++ example-sink/src/app/model/api.ts
@@ -0,0 +1,34 @@
+export interface People {
+  name: string;
+  height: string;
+  mass: string;
+  hair_color: string;
+  skin_color: string;
+  eye_color: string;
+  birth_year: string;
+  gender: string;
+  homeworld: string;
+  films: string[];
+  species: string[];
+  vehicles: string[];
+  starships: string[];
+  created: string;
+  edited: string;
+  url: string;
+}
+export interface Planet {
+  name: string;
+  rotation_period: string;
+  orbital_period: string;
+  diameter: string;
+  climate: string;
+  gravity: string;
+  terrain: string;
+  surface_water: string;
+  population: string;
+  residents: any[];
+  films: string[];
+  created: string;
+  edited: string;
+  url: string;
+}
